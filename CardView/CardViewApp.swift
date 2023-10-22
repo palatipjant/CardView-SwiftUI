5312b52c053078acd8c73f5cfbb42405e7b47e8b
Palatip Jantawong <palatipjant@outlook.com>
2023-10-22 10:02:55 +0700

    Initial Commit

---
+++ CardView/CardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewApp.swift
+//  CardView
+//
+//  Created by Palatip Jantawong on 22/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct CardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardView
+//
+//  Created by Palatip Jantawong on 22/10/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
