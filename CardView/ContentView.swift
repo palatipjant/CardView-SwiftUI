73fe03a7a8b37bbf780850794936cf9c85daeca0
Palatip Jantawong <palatipjant@outlook.com>
2023-10-24 18:30:56 +0700

    cardview

--- CardView/ContentView.swift
+++ CardView/ContentView.swift
@@ -9,11 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            // Mark: Image size adjust
+            Image(Works.works_sample.image)
+                .resizable()
+                .frame(width: 317, height: 257)
+            // Mark: Image Shadow
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(color: Color(.label).opacity(0.2), radius: 10)
+                .padding(.bottom)
+            // Mark: Text
+            VStack(alignment: .trailing) {
+                Text(Works.works_sample.category)
+                    .font(.headline)
+                    .foregroundColor(.gray)
+                    .frame(width: 310, alignment: .leading)
+                Text(Works.works_sample.heading)
+                    .multilineTextAlignment(.leading)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                    .lineLimit(3)
+                    .frame(width: 310, alignment: .leading)
+                Text(Works.works_sample.author.uppercased())
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                    .frame(width: 310, alignment: .leading)
+            }
         }
         .padding()
     }
@@ -21,4 +43,17 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct Work: Hashable, Identifiable{
+    let id = UUID()
+    let image: String
+    let category: String
+    let heading: String
+    let author: String
+    let url: String
+    
+}
+struct Works {
+    static let works_sample = Work(image: "portfolio-app", category: "Mobile", heading: "Portfolio iOS App", author: "Palatip Jantawong", url: "https://github.com/palatipjant/Portfolio-iOSApp")
 }
